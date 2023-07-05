8d85dab2b3425c681598d241034e784f63c253dc
DanielCruise <bjbjcute97@gmail.com>
2023-07-06 02:31:10 +0700

    Get item and process pricing for items

--- src/services/pricing/drink/coffee.service.ts
+++ src/services/pricing/drink/coffee.service.ts
@@ -17,32 +17,41 @@ import {
     Coffee,
     CoffeeDocument
 } from '../../../models/pricing/drink/coffee.schema';
-import {filter} from "rxjs";
+
+import pricing from '../../../const/pricing.json';
 
 @Injectable()
 export class CoffeeService {
 
-    constructor(@InjectModel(Coffee.name) private readonly coffeeModel: Model < CoffeeDocument > ) {}
+    constructor(@InjectModel(Coffee.name) private readonly coffeeModel: Model<CoffeeDocument>) {
+    }
 
-    async create(createCoffeeDto: CreateCoffeeDto): Promise < CoffeeDocument > {
+    async create(createCoffeeDto: CreateCoffeeDto): Promise<CoffeeDocument> {
         const Coffee = new this.coffeeModel(createCoffeeDto);
         return Coffee.save();
     }
 
-    async findAll(): Promise < CoffeeDocument[] > {
+    async findAll(): Promise<CoffeeDocument[]> {
         return this.coffeeModel.find()
             .exec();
     }
 
     async findOne(id: string) {
-        return this.coffeeModel.findById(id);
+        let price = 0;
+        const coffee = await this.coffeeModel.findById(id);
+        price += pricing.drink.coffee.basePrice;
+        price += pricing.drink.coffee.type[coffee.type.toLowerCase()];
+        price += pricing.drink.coffee.size[coffee.size.toLowerCase()];
+        // const itemJSON = JSON.stringify(item);
+        return {item: coffee, price: price};
+
     }
 
     async findBySize(size: string) {
         return this.coffeeModel.find({size: size})
     }
 
-    async update(id: string, updateCoffeeDto: UpdateCoffeeDto): Promise < CoffeeDocument > {
+    async update(id: string, updateCoffeeDto: UpdateCoffeeDto): Promise<CoffeeDocument> {
         return this.coffeeModel.findByIdAndUpdate(id, updateCoffeeDto);
     }
 
